ae3f32af6a070775b08b23338aeda50fb84c8b01
aqsamalic <aqsamalikurl@gmail.com>
2025-08-06 17:01:26 +0500

    Initial commit

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import emailjs from "emailjs-com";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
-import { Phone, Mail, MapPin,Instagram ,Linkedin, Facebook } from "lucide-react";
+import { Phone, Mail, MapPin, Instagram, Linkedin, Facebook } from "lucide-react";
 
-const Contact = ({ ref }) => {
+const Contact = ({ contactRef }) => {
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -27,14 +28,7 @@ const Contact = ({ ref }) => {
     setIsSubmitting(true);
 
     const templateParams = {
-      firstName: formData.firstName,
-      lastName: formData.lastName,
-      jobTitle: formData.jobTitle,
-      company: formData.company,
-      country: formData.country,
-      email: formData.email,
-      phone: formData.phone,
-      message: formData.message
+      ...formData,
     };
 
     emailjs
@@ -69,80 +63,84 @@ const Contact = ({ ref }) => {
   return (
     <div
       className="min-h-screen bg-white p-6"
-      ref={ref}
+      ref={contactRef}
       id="Contact-page"
     >
       <div className="max-w-6xl mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 gap-16">
         {/* Left Section - Contact Info */}
         <div className="space-y-6">
-          <h2 className="text-3xl font-serif font-bold text-gray-800">
+          <h2 className="text-3xl  font-bold text-gray-800">
             Get in touch<br />
             with our<br />
-            <span className="italic">specialists today</span>
+            <span className="">specialists today</span>
           </h2>
-          
+
           <div className="mt-16 space-y-10">
-           
-             <div>
-              <h3 className="font-bold  font-serif text-gray-800 text-lg mb-5">Connect With Us</h3>
-              <div className="space-y-8">
-                    
-      <a href="tel:+923084512116" className="flex items-center space-x-4">
-      <div className="bg-[#1F2456]  hover:bg-gray-800 p-3 rounded-full shadow-lg">
-        <Phone className="h-5 w-5 text-white" />
-      </div>
-      <span className="text-gray-500 hover:text-orange-500">+92 308 4512116</span>
-    </a>
-    {/* Email Link */}
-    <a href="mailto:info@microthinks.com" className="flex items-center space-x-4">
-      <div className="bg-[#1F2456] p-3 hover:bg-gray-800 rounded-full shadow-lg">
-        <Mail className="h-5 w-5 text-white" />
-      </div>
-      <span className="text-gray-500 hover:text-orange-500">info@microthinks.com</span>
-    </a>
-    {/* Location Link */}
-    <a
-     href="https://maps.app.goo.gl/tZvbk84Xks1YcW6G6"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="flex items-center space-x-4"
-    >
-      <div className="bg-[#1F2456] hover:bg-gray-800  p-3 rounded-full shadow-lg">
-        <MapPin className="h-5 w-5 text-white" />
-      </div>
-      <span className="text-gray-500 hover:text-orange-500 ">Pakistan, Lahore</span>
-    </a>
-  </div>
+            <div>
+              <h3 className="font-bold font-serif text-gray-800 text-lg mb-5">Business Information</h3>
+              <p className="text-gray-500">Microthinks Pvt Ltd</p>
+              <p className="text-gray-500">Gulberg, Lahore, Pakistan</p>
+              <p className="text-gray-500">Email: info@microthinks.com</p>
+              <p className="text-gray-500">Phone: +92 308 4512116</p>
             </div>
+
+            <div>
+              <h3 className="font-bold font-serif text-gray-800 text-lg mb-5">Connect With Us</h3>
+              <div className="space-y-8">
+                <a href="tel:+923084512116" className="flex items-center space-x-4">
+                  <div className="bg-[#1F2456] hover:bg-gray-800 p-3 rounded-full shadow-lg">
+                    <Phone className="h-5 w-5 text-white" />
+                  </div>
+                  <span className="text-gray-500 hover:text-orange-500">+92 308 4512116</span>
+                </a>
+
+                <a href="mailto:info@microthinks.com" className="flex items-center space-x-4">
+                  <div className="bg-[#1F2456] p-3 hover:bg-gray-800 rounded-full shadow-lg">
+                    <Mail className="h-5 w-5 text-white" />
+                  </div>
+                  <span className="text-gray-500 hover:text-orange-500">info@microthinks.com</span>
+                </a>
+
+                <a
+                  href="https://maps.app.goo.gl/tZvbk84Xks1YcW6G6"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center space-x-4"
+                >
+                  <div className="bg-[#1F2456] hover:bg-gray-800 p-3 rounded-full shadow-lg">
+                    <MapPin className="h-5 w-5 text-white" />
+                  </div>
+                  <span className="text-gray-500 hover:text-orange-500">Pakistan, Lahore</span>
+                </a>
+              </div>
+            </div>
+
             <div className="flex space-x-5 pt-5">
-                    
-                      
-                      
-      <a
-      href="https://facebook.com" // Replace with actual Facebook link
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-gray-500 hover:text-orange-500 transition-transform transform hover:scale-125"
-    >
-      <Facebook />
-    </a>
-    <a
-       href="https://www.linkedin.com/company/microthinks/posts/?feedView=all"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-gray-500 hover:text-orange-500 transition-transform transform hover:scale-125"
-    >
-      <Linkedin />
-    </a>
-    <a
-    href="https://www.instagram.com/microthinks?igsh=YnJ4cGJkem9zZWtx"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-gray-500 hover:text-orange-500 transition-transform transform hover:scale-125"
-    >
-      <Instagram />
-    </a>
-  </div>
+              <a
+                href="https://facebook.com/microthinks"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-500 hover:text-orange-500 transition-transform transform hover:scale-125"
+              >
+                <Facebook />
+              </a>
+              <a
+                href="https://www.linkedin.com/company/microthinks/posts/?feedView=all"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-500 hover:text-orange-500 transition-transform transform hover:scale-125"
+              >
+                <Linkedin />
+              </a>
+              <a
+                href="https://www.instagram.com/microthinks"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-500 hover:text-orange-500 transition-transform transform hover:scale-125"
+              >
+                <Instagram />
+              </a>
+            </div>
           </div>
         </div>
 
@@ -162,7 +160,7 @@ const Contact = ({ ref }) => {
                   required
                 />
               </div>
-              
+
               <div className="space-y-2">
                 <label htmlFor="lastName" className="font-medium">Last Name <span className="text-red-500">*</span></label>
                 <input
@@ -190,7 +188,7 @@ const Contact = ({ ref }) => {
                   required
                 />
               </div>
-              
+
               <div className="space-y-2">
                 <label htmlFor="company" className="font-medium">Company <span className="text-red-500">*</span></label>
                 <input
@@ -205,8 +203,6 @@ const Contact = ({ ref }) => {
               </div>
             </div>
 
-            
-
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="space-y-2">
                 <label htmlFor="email" className="font-medium">Business Email <span className="text-red-500">*</span></label>
@@ -220,7 +216,7 @@ const Contact = ({ ref }) => {
                   required
                 />
               </div>
-              
+
               <div className="space-y-2">
                 <label htmlFor="phone" className="font-medium">Phone</label>
                 <input
@@ -256,6 +252,12 @@ const Contact = ({ ref }) => {
                 {isSubmitting ? "Submitting..." : "Submit"}
               </button>
             </div>
+
+            {/* Privacy Notice */}
+           <p className="text-sm text-gray-400 mt-4">
+  By submitting this form, you agree to our
+  <Link to="/PrivacyPolicy" className="text-blue-600 underline ml-1">Privacy Policy</Link>.
+</p>
           </form>
         </div>
       </div>
